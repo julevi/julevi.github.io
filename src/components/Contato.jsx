c46768e71a75bceee2dd93ae23d5a9c4a0c19c67
Juliana <jprado.contact@gmail.com>
2024-04-19 14:55:00 -0300

    Contato atualizado

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser'
+import desenho from '../assets/images/phone.png'
 
 const Contato = () => {
-    return (
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
 
+    function sendEmail(e){
+        e.preventDefault();
+    
+        const templateParams = {
+            from_name: name,
+            email: email,
+            message: message,
+        }
+    
+        emailjs.send("service_b8kwc1h", "template_cmbjmm4", templateParams, "sLAPRyPwN4Bl6etQF")
+        .then((response) => {
+            alert("Email enviado com sucesso! " + response.status + " " + response.text);
+            setEmail('');
+            setName('');
+            setMessage('');
+        })
+        .catch((error) => {
+            console.error("Erro ao enviar o email:", error);
+            alert("Falha ao enviar o email, erro: " + error.message);
+        });
+    }
+
+    return (
         <section className="fundo1">
-            <div className="container max-w-7xl mx-auto py-10">
-                <h1 className='text-4xl mb-10 font-bold text-center'>Contato</h1>
-                <div className='flex justify-center items-center'>
-                       <p>oi oi oi</p>
+            <div className="container max-w-5xl mx-auto py-10">
+
+                <div className='flex flex-row justify-center items-center gap-28'>
+                    <img className='hidden lg:block w-96' src={desenho} alt="desenho" />
+                    <form onSubmit={sendEmail} className='flex flex-col justify-center items-center w-full px-5'>
+
+                        <h1 className='text-4xl mb-5 font-bold text-center'>Contato</h1>
+                        <input className='caixaForm my-2 w-full' type="text" id="nome" name="nome" placeholder="Nome:" onChange={(e) => setName(e.target.value)} value={name} required />
+
+                        <input className='caixaForm my-2 w-full' type="email" id="email" name="email" placeholder="Email:" onChange={(e) => setEmail(e.target.value)} value={email} required />
+
+                        <textarea className='caixaForm my-2 w-full' id="mensagem" name="mensagem" placeholder="Digite sua mensagem:" rows="8" onChange={(e) => setMessage(e.target.value)} value={message} required></textarea>
+
+                        <button type="submit" className='botao1 mt-5 animacaoToque'>Enviar</button>
+
+                    </form>
                 </div>
             </div>
         </section >
-
-
-
     );
 }
 
--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -4,7 +4,7 @@ const Rodape = () => {
     return (
 
         <footer className="menu absolute w-full z-10">
-            <div className='container max-w-4xl mx-auto py-2 px-10'>
+            <div className='container max-w-4xl mx-auto py-2'>
                 <div className='flex items-center justify-center pt-5 flex-col md:flex-row'>
 
                     <div className='flex items-center justify-between w-full md:w-auto flex-col md:flex-row'>
@@ -12,7 +12,7 @@ const Rodape = () => {
                     </div>
 
                 </div>
-                <a href="https://www.linkedin.com/in/jpradoweb/"><p className='text-sm text-white text-center md:mr-4 my-2 md:my-0 cursor-pointer relative animacaoToque'><i>Desenvolvido por @jpradoweb</i> </p></a>
+                <a href="https://www.linkedin.com/in/jpradoweb/"><p className='text-sm text-white text-center my-2 md:my-0 cursor-pointer animacaoToque'><i>Desenvolvido por @jpradoweb</i> </p></a>
             </div>
         </footer>
 
